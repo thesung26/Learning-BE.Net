916cb6040aba23a5602ff9c852032323726d3c67
the pham <the_pv@hc-techso.com>
2023-08-24 22:26:25 +0700

    ForgeAPI init proj

---
+++ TestAutodeskForge/Models/ForgeService.cs
@@ -0,0 +1,13 @@
+﻿public partial class ForgeService
+{
+    private readonly string _clientId;
+    private readonly string _clientSecret;
+    private readonly string _bucket;
+
+    public ForgeService(string clientId, string clientSecret, string bucket = null)
+    {
+        _clientId = clientId;
+        _clientSecret = clientSecret;
+        _bucket = string.IsNullOrEmpty(bucket) ? string.Format("{0}-basic-app", _clientId.ToLower()) : bucket;
+    }
+}
