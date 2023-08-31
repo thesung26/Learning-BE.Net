97ebfabc74baf011c7a64adf9e72a2fa22a46efd
the pham <the_pv@hc-techso.com>
2023-09-01 02:26:50 +0700

    database helper

---
+++ LearningBackEnd/LearningBackEnd/Database/DBHelper.cs
@@ -0,0 +1,35 @@
+﻿using LearningBackEnd.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningBackEnd.Database
+{
+    public class DBHelper
+    {
+        /*
+         * Servername:
+         * Catalog
+         * User name
+         * Password
+        */
+        static string db_address = "Data Source";
+        public static List<Product> ProductGetList()
+        {
+            var list = new List<Product>();
+
+            var sql = new SqlConnection(db_address);
+            if (sql.State == System.Data.ConnectionState.Closed)
+            {
+                sql.Open();
+            }
+
+            var cmd = new SqlCommand();            
+
+            return list;
+        }
+    }
+}
