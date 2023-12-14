63aed1acb787028003526389560b0f8c557cd15c
thesung <thesung96@gmail.com>
2023-12-14 15:03:23 +0700

    test open app on client

--- LearningBackEnd/Shop/Controllers/HomeController.cs
+++ LearningBackEnd/Shop/Controllers/HomeController.cs
@@ -4,29 +4,30 @@ using System.Diagnostics;
 
 namespace Shop.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            ViewBag.NumProduct = ProductModel.GetNumberProduct();
-            return View();
-        }
+		public IActionResult Index()
+		{
+			//ViewBag.NumProduct = ProductModel.GetNumberProduct();
+			return View();
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-  
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
